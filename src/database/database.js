37e0536983f0d9f6a99a83317874903ebb961374
Aznaya <jonasantunes@gec.inatel.br>
2022-12-10 00:14:43 -0300

    Troca de nome da conexão com o banco

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import * as SQLite from 'expo-sqlite';
 
 // Conexão com o Banco de dados sqlite
-export const Db = {
+export const Database = {
     getConnection: () => SQLite.openDatabase("database.db"),
 };
